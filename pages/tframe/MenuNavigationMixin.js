0e97e6a3f5a5314c969f72c6786f4a4a44ecec6e
kageyama-aoi <kisaragisouma09@gmail.com>
2026-04-03 18:13:31 +0900

    refactor: use shared menu navigation methods for master page

--- pages/tframe/MenuNavigationMixin.js
+++ pages/tframe/MenuNavigationMixin.js
@@ -9,6 +9,25 @@ function sanitizeName(value) {
 
 module.exports = function createMenuNavigationMixin(prefix) {
   return {
+    async verifyMenuNavigation(menuDefinition) {
+      for (const group of menuDefinition.groups) {
+        for (const item of group.items) {
+          await this.clickMenuItemAndVerify(item);
+        }
+      }
+    },
+
+    async clickMenuItemAndVerify(item) {
+      I.say(`【子メニュー押下】${item.name}`);
+      this.scrollToHref(item.href);
+      I.waitForElement(locate(`a[href="${item.href}"]`), 10);
+      this.clickLinkByHref(item.href);
+      const currentUrl = await this.waitForCurrentUrlMatch(item.href, 10);
+      this.assertCurrentUrlMatches(currentUrl, item.href);
+      I.saveScreenshotWithTimestamp(this.buildScreenshotName(item.name), true);
+      await this.clickSearchIfPresentAndCapture(item.name);
+    },
+
     buildScreenshotName(itemName) {
       return `${prefix}_${sanitizeName(itemName)}.png`;
     },
